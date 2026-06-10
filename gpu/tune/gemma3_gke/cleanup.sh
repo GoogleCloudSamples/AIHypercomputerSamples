321474e880b0d427e7c21197d937dc81bc12163c
Armando Miani <armadom@google.com>
2026-06-11 00:53:35 +0200

    chore: removing unused folder

--- gpu/tune/gemma3_gke/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source 0_env.sh
-
-set -x
-
-kubectl delete job finetune-job --ignore-not-found=true || true
-gcloud container clusters delete "${CLUSTER_NAME}" \
-    --region="${REGION}" --quiet || true
