ac5d1001ca0952792bf68798f7920b2dc272a539
Armando Miani <armadom@google.com>
2026-06-12 16:02:19 +0200

    refactor(gemma3_gke): remove automated IAM setup from cluster setup
    - Removed IAM policy binding loop from 1_setup_cluster.sh. IAM configuration is now assumed to be an external prerequisite.
    - Renumbered subsequent steps.
    
    TAG=agy
    CONV=0a08c38c-f11e-4018-a226-ca93c656ca56

--- gpu/finetune/gemma3_gke/1_setup_cluster.sh
+++ gpu/finetune/gemma3_gke/1_setup_cluster.sh
@@ -4,23 +4,7 @@ source 0_env.sh
 set -e
 set -x
 
-# 1. IAM
-declare -a ROLES=(
-  "roles/compute.admin"
-  "roles/storage.admin"
-  "roles/iam.serviceAccountUser"
-  "roles/artifactregistry.admin"
-  "roles/cloudbuild.builds.editor"
-  "roles/serviceusage.serviceUsageAdmin"
-)
-
-for i in "${ROLES[@]}"; do
-  gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
-    --member="user:${USER_EMAIL}" \
-    --role="$i"
-done
-
-# 2. Create Cluster
+# 1. Create Cluster
 # [START hypercomputer_gpu_tune_gemma3_gke_create_cluster]
 gcloud container clusters create-auto "${CLUSTER_NAME}" \
     --project="${PROJECT_ID}" \
@@ -28,20 +12,20 @@ gcloud container clusters create-auto "${CLUSTER_NAME}" \
     --release-channel=rapid
 # [END hypercomputer_gpu_tune_gemma3_gke_create_cluster]
 
-# 3. Get Creds
+# 2. Get Creds
 # [START hypercomputer_gpu_tune_gemma3_gke_get_creds]
 gcloud container clusters get-credentials "${CLUSTER_NAME}" \
     --location="${CLUSTER_REGION}"
 # [END hypercomputer_gpu_tune_gemma3_gke_get_creds]
 
-# 4. Create HF Secret
+# 3. Create HF Secret
 # [START hypercomputer_gpu_tune_gemma3_gke_create_secret]
 kubectl create secret generic hf-secret \
     --from-literal=hf_api_token="${HF_TOKEN}" \
     --dry-run=client -o yaml | kubectl apply -f -
 # [END hypercomputer_gpu_tune_gemma3_gke_create_secret]
 
-# 5. Create Artifact Registry
+# 4. Create Artifact Registry
 # [START hypercomputer_gpu_tune_gemma3_gke_create_repo]
 gcloud artifacts repositories create gemma \
     --repository-format=docker \
