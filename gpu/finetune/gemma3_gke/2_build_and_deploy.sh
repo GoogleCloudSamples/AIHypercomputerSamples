2e2672c45f38b1f7a318eca8a930f34bb3eaae14
Armando Miani <armadom@google.com>
2026-06-12 15:09:02 +0200

    docs(gemma3_gke): add region tags to build and deploy script
    Added start/end region tags for documentation snippets (build_submit, deploy_job) to allow automatic embedding in technical documentation.
    
    TAG=agy
    CONV=0a08c38c-f11e-4018-a226-ca93c656ca56

--- gpu/finetune/gemma3_gke/2_build_and_deploy.sh
+++ gpu/finetune/gemma3_gke/2_build_and_deploy.sh
@@ -5,7 +5,11 @@ set -e
 set -x
 
 # 1. Build and Submit
+# [START hypercomputer_gpu_tune_gemma3_gke_build_submit]
 gcloud builds submit .
+# [END hypercomputer_gpu_tune_gemma3_gke_build_submit]
 
 # 2. Deploy Job
+# [START hypercomputer_gpu_tune_gemma3_gke_deploy_job]
 envsubst < finetune.yaml | kubectl apply -f -
+# [END hypercomputer_gpu_tune_gemma3_gke_deploy_job]
