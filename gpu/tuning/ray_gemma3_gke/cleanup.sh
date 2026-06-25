6fa47e182268332b88ba33b62e3980db8f8ce9f1
Adrian Manikowski <manikowski@google.com>
2026-06-25 06:27:41 +0000

    The file with snippets has been corrected

--- gpu/tuning/ray_gemma3_gke/cleanup.sh
+++ gpu/tuning/ray_gemma3_gke/cleanup.sh
@@ -16,27 +16,37 @@
 
 set -euo pipefail
 
-# [START hypercomputer_gpu_tune_gemma3_ray_cleanup]
+# [START hypercomputer_gpu_tune_gemma3_ray_cleanup_ray_job]
 kubectl delete -f ray_job.yaml --ignore-not-found=true || true
 echo "[$(date)] ==================== Ray job deleted. ===================="
+# [END hypercomputer_gpu_tune_gemma3_ray_cleanup_ray_job]
 
+# [START hypercomputer_gpu_tune_gemma3_ray_cleanup_configmap]
 kubectl delete configmap ray-job-cm --ignore-not-found=true || true
 echo "[$(date)] ==================== ConfigMap deleted. ===================="
+# [END hypercomputer_gpu_tune_gemma3_ray_cleanup_configmap]
 
+# [START hypercomputer_gpu_tune_gemma3_ray_cleanup_ray_cluster]
 kubectl delete -f ray_cluster.yaml --ignore-not-found=true || true
 echo "[$(date)] ==================== Ray cluster deleted. ===================="
+# [END hypercomputer_gpu_tune_gemma3_ray_cleanup_ray_cluster]
 
+# [START hypercomputer_gpu_tune_gemma3_ray_cleanup_gke_cluster]
 gcloud container clusters delete "${CLUSTER_NAME}" \
     --region="${REGION}" --quiet || true
 echo "[$(date)] ==================== GKE cluster deleted. ===================="
+# [END hypercomputer_gpu_tune_gemma3_ray_cleanup_gke_cluster]
 
+# [START hypercomputer_gpu_tune_gemma3_ray_cleanup_gcs_bucket]
 if gcloud storage buckets describe gs://"${GCS_BUCKET}" > /dev/null 2>&1; then
     gcloud storage rm -r gs://"${GCS_BUCKET}"
     echo "[$(date)] ==================== GCS bucket deleted. ===================="
 else
     echo "[$(date)] ==================== GCS bucket did not exist (Skipping). ===================="
 fi
+# [END hypercomputer_gpu_tune_gemma3_ray_cleanup_gcs_bucket]
 
+# [START hypercomputer_gpu_tune_gemma3_ray_cleanup_sa]
 gcloud iam service-accounts delete $GSA_NAME@$PROJECT_ID.iam.gserviceaccount.com --project=$PROJECT_ID --quiet || true
 echo "[$(date)] ==================== Google Service Account deleted. ===================="
-# [END hypercomputer_gpu_tune_gemma3_ray_cleanup]
+# [END hypercomputer_gpu_tune_gemma3_ray_cleanup_sa]
