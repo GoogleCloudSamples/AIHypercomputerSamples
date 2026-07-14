8cf5506dad67d107bc6bfac19dd4353561348fbe
Adrian Manikowski <manikowski@google.com>
2026-07-14 09:31:40 +0000

    Updated docs_snippets bad indent

--- gpu/tuning/ray_gemma3_gke/docs.snippets.sh
+++ gpu/tuning/ray_gemma3_gke/docs.snippets.sh
@@ -29,7 +29,7 @@ kubectl delete -f ray_cluster.yaml
 # [END hypercomputer_gpu_tune_gemma3_ray_delete_ray_cluster]
 
 # [START hypercomputer_gpu_tune_gemma3_ray_delete_gke_cluster]
-    gcloud container clusters delete $CLUSTER_NAME \
+gcloud container clusters delete $CLUSTER_NAME \
     --region=$REGION
 # [END hypercomputer_gpu_tune_gemma3_ray_delete_gke_cluster]
 
