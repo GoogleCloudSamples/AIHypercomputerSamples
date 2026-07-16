ec4f9c4f163f4a66fadd439555c34519c2612ca8
Armando Miani <armadom@google.com>
2026-07-16 21:11:47 +0200

    Update docs_scripts.sh

--- gpu/tuning/verl_rl_autopilot/docs_scripts.sh
+++ gpu/tuning/verl_rl_autopilot/docs_scripts.sh
@@ -1,3 +1,11 @@
 # [START hypercomputer_gpu_train_ray_verl_clone]
 git clone https://github.com/GoogleCloudSamples/AIHypercomputerSamples.git
 # [END hypercomputer_gpu_train_ray_verl_clone]
+
+# [START hypercomputer_gpu_train_ray_chdir_autopilot]
+cd gpu/tuning/verl_rl_autopilot
+# [END hypercomputer_gpu_train_ray_chdir_autopilot]
+
+# [START hypercomputer_gpu_train_ray_chdir_std]
+cd gpu/tuning/verl_rl_standard
+# [END hypercomputer_gpu_train_ray_chdir_std]
