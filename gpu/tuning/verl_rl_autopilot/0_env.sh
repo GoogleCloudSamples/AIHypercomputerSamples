dc78b0083c0b00f7c0632bd1375ec224c273e8b6
Armando Miani <armadom@google.com>
2026-08-28 14:13:44 +0200

    chore: move WORKER_TIMEOUT to env soo it can be parameterized

--- gpu/tuning/verl_rl_autopilot/0_env.sh
+++ gpu/tuning/verl_rl_autopilot/0_env.sh
@@ -35,4 +35,7 @@ export HF_TOKEN="YOUR_HF_TOKEN"
 # GPU node provisioning delays.
 # Can be parameterized by setting --tpu_sample_var=WORKER_TIMEOUT=1200s
 # for example.
-export WORKER_TIMEOUT="${WORKER_TIMEOUT:-1320s}"
+export WORKER_TIMEOUT="YOUR_WORKER_TIMEOUT"
+if [[ "${WORKER_TIMEOUT}" == "YOUR_WORKER_TIMEOUT" ]]; then
+  export WORKER_TIMEOUT="1320s"
+fi
