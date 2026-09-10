7dbb329455a423f62d3c9a078821f8472037b043
Armando Miani <armadom@google.com>
2026-09-10 10:01:42 +0200

    fix(tpu/sft_tuning_gke_tpu_gcluster): remove unused REPOSITORY_NAME from v2 env snippet
    - Remove REPOSITORY_NAME export in hypercomputer_tpu_sft_gcluster_env_v2 since pre-built image is used directly
    
    Bug: b/558523611
    TAG=agy
    CONV=fc091177-9c7c-4c94-8794-b4409c14bc9e

--- tpu/tuning/sft_tuning_gke_tpu_gcluster/0_env.sh
+++ tpu/tuning/sft_tuning_gke_tpu_gcluster/0_env.sh
@@ -36,7 +36,6 @@ export PROJECT="YOUR_PROJECT_ID"
 export REGION="YOUR_REGION"
 export ZONE="YOUR_ZONE"
 export CLUSTER_NAME="gke-tpu-v6e"
-export REPOSITORY_NAME="YOUR_REPOSITORY_NAME"
 export GCS_BUCKET="YOUR_BUCKET_NAME"
 export CLOUD_IMAGE_NAME="us-docker.pkg.dev/cloud-tpu-images/maxtext-images/tpu_post_training:0.2.4"
 export TPU_TYPE="v6e-32"
@@ -44,4 +43,3 @@ export RESERVATION="YOUR_RESERVATION_NAME"
 export MODEL_NAME="gemma4-31b"
 export HF_TOKEN="YOUR_HF_TOKEN"
 # [END hypercomputer_tpu_sft_gcluster_env_v2]
-
